6bdb3f0612678e35a263f3992300771e90ce75bc
ThanhNha <72262090+ThanhNha@users.noreply.github.com>
2022-05-13 15:49:00 +0700

    done page service

--- script.js
+++ script.js
@@ -118,3 +118,47 @@ function header() {
   });
 }
 header();
+
+$(".wrapper-cards-flow.mobile").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  dots: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: "40px",
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: "20px",
+      },
+    },
+  ],
+});
